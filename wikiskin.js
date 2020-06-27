cbea5f9aa2028b8354d33547c0a69a36769882cd
Ryan <lolwows111@gmail.com>
2020-06-26 17:18:36 -0700

    Added js
    Copied from a stackexchange answer. Supports no customization.

--- wikiskin.js
+++ wikiskin.js
@@ -1,1 +1,6 @@
-a
+if ('URLSearchParams' in window) {
+    var searchParams = new URLSearchParams(window.location.search)
+    searchParams.set("useskin", "timeless");
+    var newRelativePathQuery = window.location.pathname + '?' + searchParams.toString();
+    history.pushState(null, '', newRelativePathQuery);
+}
