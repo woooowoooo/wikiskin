dee696d54f7790bdae07012099f2a18a6f5b5d4b
Ryan <lolwows111@gmail.com>
2020-06-26 23:57:55 -0700

    Updated scripts
    Realized that my popup script wasn't even complete. As of now the buttons do nothing and the extension automatically sets wikipedia to "timeless".

--- wikiskin.js
+++ wikiskin.js
@@ -1,7 +1,11 @@
-document.body.style.border = "5px solid red";
+console.log("Wikiskin test!");
+document.body.style.border = "5px dashed orange";
 if ("URLSearchParams" in window) {
-    var searchParams = new URLSearchParams(window.location.search)
-    searchParams.set("useskin", "timeless");
-    var newRelativePathQuery = window.location.pathname + "?" + searchParams.toString();
-    history.pushState(null, "", newRelativePathQuery);
+	var searchParams = new URLSearchParams(window.location.search);
+	if (searchParams.get("useskin") != "timeless") {
+		searchParams.set("useskin", "timeless");
+		window.location.replace(window.location.pathname + "?" + searchParams.toString());
+	}
+} else {
+	console.log("Wow, you are on an old browser.");
 }
