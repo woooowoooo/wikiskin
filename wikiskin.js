81398124aded70f699cb273dcaa0192fa6ab95db
Ryan <lolwows111@gmail.com>
2020-06-26 17:56:38 -0700

    Added popup stuff
    Other minor changes too.

--- wikiskin.js
+++ wikiskin.js
@@ -1,6 +1,7 @@
-if ('URLSearchParams' in window) {
+document.body.style.border = "5px solid red";
+if ("URLSearchParams" in window) {
     var searchParams = new URLSearchParams(window.location.search)
     searchParams.set("useskin", "timeless");
-    var newRelativePathQuery = window.location.pathname + '?' + searchParams.toString();
-    history.pushState(null, '', newRelativePathQuery);
+    var newRelativePathQuery = window.location.pathname + "?" + searchParams.toString();
+    history.pushState(null, "", newRelativePathQuery);
 }
