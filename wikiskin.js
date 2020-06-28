13f3c342a68431eb06b463b8e9860d1d8aa2db53
Ryan <lolwows111@gmail.com>
2020-06-27 20:39:53 -0700

    Border removed
    The test is successful.

--- wikiskin.js
+++ wikiskin.js
@@ -1,5 +1,4 @@
 console.log("Wikiskin test!");
-document.body.style.border = "5px dashed orange";
 if ("URLSearchParams" in window) {
 	var searchParams = new URLSearchParams(window.location.search);
 	if (searchParams.get("useskin") != "timeless") {
